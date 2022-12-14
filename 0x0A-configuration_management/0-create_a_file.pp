2e381925886ac53329a394365ed11ed79fb43ce4
RaymanMoha <105370124+RaymanMoha@users.noreply.github.com>
2022-12-14 09:44:31 +0300

    Create 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Using puppet, create a file in /tmp
+file { 'school':
+  ensure  => present,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
