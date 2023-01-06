d9d904baabbfad66875d6ccdc4cf82e519ee7a87
RaymanMoha <105370124+RaymanMoha@users.noreply.github.com>
2023-01-06 12:42:37 +0300

    puppet config

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,21 @@
-# connect to a server Execute a command
+# Client configuration file (w/ Puppet)
+# Your SSH client configuration must be configured to use
+# the private key ~/.ssh/school
+# Your SSH client configuration must be configured to refuse
+# to authenticate using a password
+include stdlib
+file_line { 'NO password':
+    ensure             => present,
+    path               => '/etc/ssh/ssh_config',
+    line               => '    PasswordAuthentication no',
+    match              => '^PasswordAuthentication',
+    append_on_no_match => true,
+  }
 
-exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
-        path    => '/bin/'
-}
+file_line { 'New private key holder':
+    ensure             => present,
+    path               => '/etc/ssh/ssh_config',
+    line               => '    IdentityFile ~/.ssh/school',
+    match              => '^IdentityFile',
+    append_on_no_match => true,
+  }
